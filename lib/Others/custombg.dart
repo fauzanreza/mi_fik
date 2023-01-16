8843810b4310321325dc34f5ba52710506011d9b
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2023-01-16 23:53:15 +0700

    login page
    FE only

--- lib/Others/custombg.dart
+++ lib/Others/custombg.dart
@@ -56,3 +56,85 @@ class CirclePainter extends CustomPainter {
     return true;
   }
 }
+
+//Login Page
+class CirclePainterSide extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Customable paint path
+    var paint = Paint()..strokeWidth = 15;
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, size.width, size.height));
+    paint.color = primaryColor;
+    canvas.drawPath(mainBackground, paint);
+
+    //Color attribute
+    var paint1 = Paint()..color = primaryColor; //Main color
+    var paint2 = Paint()
+      ..color = const Color.fromARGB(255, 192, 115, 0); //Border
+
+    //Right circle
+    canvas.drawCircle(
+        Offset(size.width * 0.92, size.height * 0.06), 50, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.92, size.height * 0.06), 48.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.92, size.height * 0.06), 44, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.92, size.height * 0.06), 42.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.92, size.height * 0.06), 38, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.92, size.height * 0.06), 36.5, paint1);
+
+    canvas.drawCircle(
+        Offset(size.width * 0.82, size.height * 0.84), 50, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.82, size.height * 0.84), 48.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.82, size.height * 0.84), 44, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.82, size.height * 0.84), 42.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.82, size.height * 0.84), 38, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.82, size.height * 0.84), 36.5, paint1);
+
+    //Left circle
+    canvas.drawCircle(
+        Offset(size.width * 0.07, size.height * 0.25), 50, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.07, size.height * 0.25), 48.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.07, size.height * 0.25), 44, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.07, size.height * 0.25), 42.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.07, size.height * 0.25), 38, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.07, size.height * 0.25), 36.5, paint1);
+
+    canvas.drawCircle(
+        Offset(size.width * 0.13, size.height * 0.88), 50, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.13, size.height * 0.88), 48.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.13, size.height * 0.88), 44, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.13, size.height * 0.88), 42.5, paint1);
+    canvas.drawCircle(
+        Offset(size.width * 0.13, size.height * 0.88), 38, paint2);
+    canvas.drawCircle(
+        Offset(size.width * 0.13, size.height * 0.88), 36.5, paint1);
+
+    // paint.color = const Color(0xFF0a0c10);
+
+    // canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
